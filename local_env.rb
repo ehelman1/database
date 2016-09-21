f3af165752af1ec672ff8281afe26324b1dc427a
ehelman1 <eddhelman@gmail.com>
2016-09-21 12:36:01 -0400

    Delete local_env.rb

--- local_env.rb
+++
@@ -1,6 +0,0 @@
-ENV['dbuser']='database1'
-ENV['dbpassword']='database123'
-ENV['domain']='localhost'
-ENV['dbport']= '5432'
-ENV['dbname']='testdatabase'
-ENV['db']='testdatabase.cpdneptoktvs.us-west-2.rds.amazonaws.com'
